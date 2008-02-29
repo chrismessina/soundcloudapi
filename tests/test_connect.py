9e73ebbab186b869e6811cd32cb5ae01a5c5f1cb
diez.roggisch <diez.roggisch@454f940a-2646-0410-b391-ad9ffe2c847f>
2008-02-29 17:50:38 +0000

    pushed forward the oauth-implementation - but some stuff to do server-side

--- tests/test_connect.py
+++ tests/test_connect.py
@@ -189,9 +189,15 @@ def test_favorites():
 
 def test_large_list():
     sca = scapi.Scope()
+    me = sca.me()
     tracks = sca.tracks()
     if len(tracks) < scapi.SoundCloudAPI.LIST_LIMIT:
         for i in xrange(scapi.SoundCloudAPI.LIST_LIMIT):            
             track = scapi.Track.new(title='test_track_%i' % i)
     all_tracks = sca.tracks()
     assert len(all_tracks) > scapi.SoundCloudAPI.LIST_LIMIT
+
+
+def test_auth():
+    sca = scapi.Scope()
+    me = sca.me()
